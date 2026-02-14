9211831c67e8b3134aaf06f57d32fec201baa2bc
westonz7042 <westonz7042@gmail.com>
2026-02-13 17:10:06 -0800

    gh actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/valentines-day',
   images: {
     formats: ["image/avif", "image/webp"],
+    unoptimized: true,
   },
 };
 
